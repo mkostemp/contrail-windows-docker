dc6ba7351db8f7ad7749c45a5f919957f276138e
m-kostrzewa <kostrzewa.michal@o2.pl>
2017-01-09 15:31:05 +0100

    Branch testing improvements.
    Lets specify branch to checkout. If repo exists, won't clone but pull.
    
    Saves test logs to C:\testresults.txt.

--- ci/clone_and_test.ps1
+++ ci/clone_and_test.ps1
@@ -1,9 +1,19 @@
 # Clones docker network driver repo and runs tests
 
-echo "Cloning code repo github.com/codilime/contrail-windows-docker "
-New-Item -ItemType directory -Path C:\go_workspace\src\github.com
-New-Item -ItemType directory -Path C:\go_workspace\src\github.com\codilime
-git clone https://github.com/codilime/contrail-windows-docker C:\go_workspace\src\github.com\codilime\contrail-windows-docker
+$branch=$args[0]
+
+$repoExists = Test-Path C:\go_workspace\src\github.com\codilime\contrail-windows-docker
+if($repoExists -eq $True) {
+    echo "Checking out $branch from github.com/codilime/contrail-windows-docker"
+    cd C:\go_workspace\src\github.com\codilime\contrail-windows-docker
+    git pull origin $branch
+    git checkout $branch
+} else {
+    echo "Cloning code repo github.com/codilime/contrail-windows-docker, branch $branch"
+    New-Item -ItemType directory -Path C:\go_workspace\src\github.com
+    New-Item -ItemType directory -Path C:\go_workspace\src\github.com\codilime
+    git clone https://github.com/codilime/contrail-windows-docker -b $branch C:\go_workspace\src\github.com\codilime\contrail-windows-docker
+}
 
 echo "Building docker network driver"
 cd C:\go_workspace\bin
@@ -15,4 +25,7 @@ go get github.com/onsi/gomega
 
 echo "Running tests"
 cd C:\go_workspace\src\github.com\codilime\contrail-windows-docker
+
+Start-Transcript -path C:\testresults.txt
 C:\go_workspace\bin\ginkgo.exe -r .
+Stop-Transcript
