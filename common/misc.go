6a5f56e3c43094e48c4bae4b1d2a6b0a83d0d01d
m-kostrzewa <kostrzewa.michal@o2.pl>
2017-01-31 20:05:38 +0100

    Improve HNS cleanup.

--- common/misc.go
+++ common/misc.go
@@ -1,15 +1,23 @@
 package common
 
 import (
+	"errors"
+	"fmt"
 	"os/exec"
 
 	log "github.com/Sirupsen/logrus"
 )
 
 func HardResetHNS() error {
+	log.Infoln("Resetting HNS")
+	log.Debugln("Removing NAT")
+	if err := exec.Command("powershell", "Get-NetNat", "|",
+		"Remove-NetNat").Run(); err != nil {
+		log.Debugln("Could not remove container network.")
+	}
 	log.Debugln("Removing container networks")
 	if err := exec.Command("powershell", "Get-ContainerNetwork", "|",
-		"Remove-ContainerNetwork").Run(); err != nil {
+		"Remove-ContainerNetwork", "-Force").Run(); err != nil {
 		log.Debugln("Could not remove container network.")
 	}
 	log.Debugln("Stopping HNS")
@@ -19,10 +27,18 @@ func HardResetHNS() error {
 	log.Debugln("Removing HNS program data")
 	if err := exec.Command("powershell", "Remove-Item",
 		"C:\\ProgramData\\Microsoft\\Windows\\HNS\\HNS.data").Run(); err != nil {
-		return err
+		return errors.New(fmt.Sprintf("Error during removing HNS program data: %s", err))
 	}
 	log.Debugln("Starting HNS")
 	if err := exec.Command("powershell", "Start-Service", "hns").Run(); err != nil {
+		return errors.New(fmt.Sprintf("Error when starting HNS: %s", err))
+	}
+	return nil
+}
+
+func RestartDocker() error {
+	log.Infoln("Restarting docker")
+	if err := exec.Command("powershell", "Restart-Service", "docker").Run(); err != nil {
 		return err
 	}
 	return nil
