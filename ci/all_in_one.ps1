fb6a8726da5d93676b50f34afe521196f971b99b
m-kostrzewa <kostrzewa.michal@o2.pl>
2017-02-23 17:03:26 +0100

    Remove unused CI files

--- ci/all_in_one.ps1
+++
@@ -1,9 +0,0 @@
-# Sets up environment from scratch and runs tests.
-
-$branch=$args[0]
-
-Invoke-Expression "./install_git.ps1"
-Invoke-Expression "./install_golang.ps1"
-Invoke-Expression "./install_gcc.ps1"
-Invoke-Expression "./install_docker.ps1"
-Invoke-Expression "./clone_and_test.ps1 $branch"
